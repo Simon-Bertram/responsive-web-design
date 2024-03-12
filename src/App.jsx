c7dce40901d1c81747f83165f852ffdf8b7edc26
Simon-Bertram <simonbertram@hotmail.com>
2024-03-12 10:18:44 +0000

    Add header from tailwindui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-3xl">My site</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
